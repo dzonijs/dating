4b14675c62b17543949e4d572ec83d2eac59f368
AleksandarSoft <aleksandar.tomic@zoho.com>
2019-09-23 16:15:59 +0200

    initial commit

---
+++ DatingApp.API/Helpers/Extensions.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.AspNetCore.Http;
+
+namespace DatingApp.API.Helpers
+{
+    public static class Extensions
+    {
+        public static void AddAplicationError(this HttpResponse response, string message)
+        {
+            response.Headers.Add("ApplicationError", message);
+            response.Headers.Add("Access-Control-Expose-Headers", "ApplicationError");
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+
+        public static int CaluclateAge(this DateTime theDateTime)
+        {
+            var age = DateTime.Today.Year - theDateTime.Year;
+            if (theDateTime.AddYears(age) > DateTime.Today)
+            age--;
+
+            return age;
+        }
+    }
+}
